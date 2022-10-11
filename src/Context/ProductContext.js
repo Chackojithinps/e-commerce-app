8d21f237c029e68ef8f2244a324324242a9e6426
Chacko jithin <jithinchackopayyanat@gmail.com>
2022-10-11 15:45:51 +0530

    context completed

--- src/Context/ProductContext.js
+++ src/Context/ProductContext.js
@@ -1,27 +1,33 @@
 import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
 
-const ProductContext=createContext();
+export const ProductContext=createContext();
 
 export const Cart=({children})=>{
     const [productlist,setProductlist]=useState([])
+    const [loading,setLoading]=useState(false)
     useEffect(()=>{
+        setLoading(true)
         const getProducts=async()=>{
             await axios.get("https://fakestoreapi.com/products").then((res)=>{
-            // console.log(res)
+            console.log(res)
             setProductlist(res.data)
+            setLoading(false)
         })
     }
       getProducts()
     },[])
     const values={
-        productlist
+        productlist,loading
     }
     return(
         <ProductContext.Provider value={values} >{children}</ProductContext.Provider>
     )
 }
 
-export const useProduct=()=>{
-    useContext(ProductContext)
-}
+// export const useProduct=()=>{
+//     useContext(ProductContext)
+// }
+// export const useProduct=()=>{
+//     useContext(ProductContext)
+// }
--- src/Pages/Products/index.js
+++ src/Pages/Products/index.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import { useProduct } from '../../Context/ProductContext'
+import React, { useContext } from 'react'
+import { ProductContext } from '../../Context/ProductContext'
 import './styles.css'
 const Products = () => {
-    const {productlist}=useProduct()
+    const {productlist,loading} = useContext(ProductContext)
   return (
     <div>
-       
+       {
+        !loading? (productlist.map((item)=>(
+            <div>
+                <h1>{item.price}</h1>
+                <h1>{item.title}</h1>
+            </div>
+        ))):<h1>loading...</h1>
+      }
     </div>
   )
 }
