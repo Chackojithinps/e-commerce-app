d9b206c7f2c81adc00f7425c89deddd4bb0557da
Chacko jithin <jithinchackopayyanat@gmail.com>
2022-10-14 20:01:18 +0530

    deploying changes

--- src/Components/Logo/index.js
+++ src/Components/Logo/index.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
+
 import { ProductContext } from '../../Context/ProductContext'
 import './styles.css'
 const Logo = () => {
